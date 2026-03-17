6600caeccfa26f1fff8163da124926d28bf4588f
qaankitjakhmola <qa.ankitjakhmola@gmail.com>
2026-03-17 14:31:30 +0530

    login page commented

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,14 +1,14 @@
-export class Login {
-    constructor(page){
-        this.page = page;
-        this.loginCta = page.locator("#loginButton");
-        this.customerCta = page.locator('//a[contains(@title, "Customer")]');
-        this.usernameInputField = page.locator("#input-userInput");
-        this.otpInputField = page.locator("(//input[@inputmethod='numeric'])[1]");
-    }
+// export class Login {
+//     constructor(page){
+//         this.page = page;
+//         this.loginCta = page.locator("#loginButton");
+//         this.customerCta = page.locator('//a[contains(@title, "Customer")]');
+//         this.usernameInputField = page.locator("#input-userInput");
+//         this.otpInputField = page.locator("(//input[@inputmethod='numeric'])[1]");
+//     }
 
-    async userLogin(){
+//     async userLogin(){
 
-    }
+//     }
 
-}
+// }
